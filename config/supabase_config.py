cbaca536017ec4a1c1e70df8b679aa6dba035f0f
Manuel40702 <jnunesreyes@gmail.com>
2025-03-17 17:53:56 -0600

    Update content for deploy

--- config/supabase_config.py
+++ config/supabase_config.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from supabase import create_client
 
+
 def supabase_config():
     SUPABASE_URL = st.secrets["supabase"]["SUPABASE_URL"]
     SUPABASE_KEY = st.secrets["supabase"]["SUPABASE_KEY"]
